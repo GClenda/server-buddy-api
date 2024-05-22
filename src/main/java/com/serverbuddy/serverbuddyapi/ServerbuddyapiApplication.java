11f24f19acfa3a987175b4e918ad2c9f7d41fb19
Glenda <chicag9313@gmail.com>
2024-05-22 16:32:56 -0700

    Initial spring boot set up

---
+++ src/main/java/com/serverbuddy/serverbuddyapi/ServerbuddyapiApplication.java
@@ -0,0 +1,13 @@
+package com.serverbuddy.serverbuddyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerbuddyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerbuddyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serverbuddy/serverbuddyapi/ServerbuddyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverbuddy.serverbuddyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerbuddyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
